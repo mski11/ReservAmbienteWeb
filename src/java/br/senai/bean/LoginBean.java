c0f665a58d7e5e3ce5aac33dcae4c148873e4ad2
Aluno <chrystian_silva@estudante.sc.senai.br>
2019-10-16 17:14:08 -0300

    Página inicial com pedido de registro agora quase completo. Faltando: Limitação de caracteres no campo "Telefone"; Checagem se um pedido com o mesmo e-mail já não foi feito; Checar se existem campos vazios;

--- src/java/br/senai/bean/LoginBean.java
+++ src/java/br/senai/bean/LoginBean.java
@@ -1,6 +1,7 @@
 package br.senai.bean;
 
 import br.senai.dao.pedidoRegistroDAO;
+import br.senai.model.PedidoRegistro;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -8,9 +9,33 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class LoginBean {
     
+    PedidoRegistro pedidoRegistro = new PedidoRegistro();
     pedidoRegistroDAO pr = new pedidoRegistroDAO();
-
+    
+    public void salvar(){
+        pr.salvar(pedidoRegistro);
+    }
+    
     public String conectar(){
         return pr.conectar();
     }
+
+    public PedidoRegistro getPedidoRegistro() {
+        return pedidoRegistro;
+    }
+
+    public void setPedidoRegistro(PedidoRegistro pedidoRegistro) {
+        this.pedidoRegistro = pedidoRegistro;
+    }
+
+    public pedidoRegistroDAO getPr() {
+        return pr;
+    }
+
+    public void setPr(pedidoRegistroDAO pr) {
+        this.pr = pr;
+    }
+    
+    
+    
 }
